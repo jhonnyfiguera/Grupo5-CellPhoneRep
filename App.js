8dc7485182a53b49c069a5f01f8c446d8657f5f9
[patriciarodriguez87] <[patrodriguez87@gmail.com]>
2021-11-07 03:54:11 -0300

    Armado inicial de proyecto

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+import TopButtonNavigatior from './components/TopButtonNavigator';
+import StackNavigator from './components/StackNavigatior';
+import Drawer from './components/DrawerNavigator';
+
+function Main() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer name="Menu" component={TopButtonNavigatior} />
+      {/*<Drawer name="Prueba" component={StackNavigator} />*/}
+    </Drawer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+         <StackNavigator name="Home" component={Main} options={{ headerShown: false }} />
+    </NavigationContainer>
+  );
+};
