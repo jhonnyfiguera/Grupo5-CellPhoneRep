1ed94a5727236f0839618f3a25ffb6c924ddd882
jhonnyfiguera <figuerajhonny5@gmail.com>
2021-11-13 09:52:36 -0300

    Add StackNav

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import DrawerNavigator from './components/DrawerNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import TopTabsNavigator from './components/TopTabsNavigator';
+import * as Google from 'expo-auth-session/providers/google';
 
 export default function App() {
+	const Stack = createStackNavigator();
 	return (
-		<NavigationContainer>
-		
-    		 <TopTabsNavigator/> 
+		 <NavigationContainer>
+			<Stack.Navigator initialRouteName={'Login'}>
+				<Stack.Screen name={'Login'} component={Login} options={{headerShown: false}}/>
+				<Stack.Screen name={'TopTabsNavigator'} component={TopTabsNavigator} options={{headerShown: false}} />
+			</Stack.Navigator>
 		</NavigationContainer>
+	
 	);
 }
-
